79b1cf986290401cb40e7e952dffd4f2bc02b4eb
Christopher Burns <christopher@tapstudio.co.uk>
2022-03-03 14:52:59 +0000

    removed prisma db call on GraphQL Handler

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -4,7 +4,6 @@ import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
-import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
@@ -12,8 +11,4 @@ export const handler = createGraphQLHandler({
   directives,
   sdls,
   services,
-  onException: () => {
-    // Disconnect from your database with an unhandled exception.
-    db.$disconnect()
-  },
 })
